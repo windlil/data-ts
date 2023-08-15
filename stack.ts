a6be497f98ca055cfa8df1c4ff1a7066a232bd12
windlil <spacesouls@163.com>
2023-08-15 17:11:30 +0800

    feat: stack

--- stack.ts
+++ stack.ts
@@ -10,9 +10,9 @@ class ArrayStack {
     this.stack.push(value)
   }
 
-  //出栈
+  //出栈，并返回被移除的元素
   pop() {
-    this.stack.pop()
+    return this.stack.pop()
   }
 
   //返回栈顶元素，不对元素进行任何操作
@@ -34,3 +34,13 @@ class ArrayStack {
 }
 
 const s1 = new ArrayStack()
+
+console.log(s1.isEmpty())   //true
+s1.push("jack")
+s1.push("tom")
+s1.push("mike")
+console.log(s1.size())      //3
+console.log(s1.peek())      //mike
+console.log(s1.pop())       //mike
+console.log(s1.size())      //2
+console.log(s1.isEmpty())   //false
