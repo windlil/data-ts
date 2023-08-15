5ec9c9d899e3e759d5c5c543bb3b6e5b16f249c6
windlil <spacesouls@163.com>
2023-08-15 23:22:45 +0800

    chore: update

--- src/stack/面试题/有效的括号.ts
+++ src/stack/面试题/有效的括号.ts
@@ -1,5 +1,34 @@
 import { ArrayStack } from '../ArrayStack'
 
 /*
-  
+  给函数传入(){}[]组合成的字符串，判断其组合是否为有效的括号组合。
+  isValid("(){}[]") true
+  isValid("({)}[]") false
+  isValid("{()}[]") true
+  isValid("[{()}]") true
 */
+
+function isValid(str: string): boolean {
+  const stack = new ArrayStack<string>()
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i]
+
+    if (c === '{') {
+      stack.push('}')
+    } else if (c === '(') {
+      stack.push(')')
+    } else if (c === '[') {
+      stack.push(']')
+    } else {
+      if (c !== stack.pop()) return false
+    }
+  }
+
+  return stack.isEmpty()
+}
+
+console.log(isValid("(){}[]"))  // true
+console.log(isValid("({)}[]"))  // false [')', '}']
+console.log(isValid("{()}[]") ) // true
+console.log(isValid("[{()}]"))  // true
