5491cb256717eeaffa6f6908e598b29b638ba313
windlil <windlil@outlook.com>
2023-09-11 18:03:36 +0800

    feat: update

--- src/4_hash/index.ts
+++ src/4_hash/index.ts
@@ -1,3 +1,13 @@
+function isPrime(num: number): boolean {
+  const sqrt = Math.sqrt(num)
+
+  for (let i = 2; i <= sqrt; i++) {
+    if (sqrt % 2 === 0) {
+      return false
+    }
+  }
+  return true
+}
 
 class hashTable<T = any> {
   private storage: [string, T][][] = []
@@ -35,6 +45,21 @@ class hashTable<T = any> {
     }
   }
 
+  private resize(newLength: number) {
+    this.length = newLength
+    const oldStorage = this.storage
+    this.storage = []
+    this.count = 0
+
+    oldStorage.forEach((bucket: [string, T][]) => {
+      if (!bucket) return
+      for (let i = 0; i < bucket.length; i++) {
+        const tuple = bucket[i]
+        this.put(tuple[0], tuple[1])
+      }
+    })
+  }
+
   put(key: string, value: T) {
     const index = this.getIndex(key)
     let bucket = this.getBucket(index)
@@ -53,6 +78,11 @@ class hashTable<T = any> {
     if (!isUpdate) {
       bucket.push([key, value])
       this.count++
+
+      const loadFactor = this.count / this.length
+      if (loadFactor > 0.75) {
+        this.resize(this.length * 2)
+      }
     }
   }
 
@@ -88,6 +118,11 @@ class hashTable<T = any> {
         const deleteValue = tuple[1]
         tuple.splice(i, 1)
         this.count-- 
+
+        const loadFactor = this.count / this.length
+        if (loadFactor < 0.25 && this.length > 7) {
+          this.resize(Math.floor(this.length / 2))
+        }
         return deleteValue
       }
     }, this)
