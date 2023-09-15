325e766da556a9bb839a4a7eb043ded65e69f219
windlil <windlil@outlook.com>
2023-09-15 15:58:36 +0800

    chore: update

--- src/7_heap/index.ts
+++ src/7_heap/index.ts
@@ -8,6 +8,12 @@ class Heap {
     this.data[n2] = temp
   }
 
+  constructor(arr?: number[]) {
+    if (arr) {
+       this.build(arr)
+    }
+  }
+
   insert(value: number) {
     if (this.data.length === 0) {
       this.data.push(value)
@@ -28,16 +34,54 @@ class Heap {
     this.length++
   }
 
+  build(arr: number[]) {
+    this.data = arr
+    this.length = arr.length
+    let start = Math.floor((this.length - 1) / 2) 
+    for (let i = start; i >= 0; i--) {
+      this.xialv(i)
+    }
+  }
+
+  extract(): number | undefined {
+    if (this.length === 0) return undefined
+    if (this.length === 1) {
+      this.length--
+      return this.data.pop()
+    }
+    const current = this.data[0]
+    this.data[0] = this.data.pop()!
+    this.length --
+
+    this.xialv(0)
+    return current
+  }
+
+  xialv(start: number) {
+    let index = start
+    while(2 * index + 1 <  this.length) {
+      let leftIndex = index * 2 + 1
+      let rightIndex = leftIndex + 1
+      
+      let max = leftIndex
+      if (this.data[rightIndex] && this.data[rightIndex] > this.data[leftIndex]) {
+        max = rightIndex
+      }
+  
+      if (this.data[max] > this.data[index]) {
+        this.swap(max, index)
+        index = max
+      } else {
+        break
+      }
+    }
+  }
+
   print() {
     console.log(this.data)
   }
 }
 
-const heap = new Heap()
-heap.insert(20)
-heap.insert(25)
-heap.insert(18)
-heap.insert(30)
-
+const heap = new Heap([19, 100, 36, 17, 3, 25, 1, 2, 7])
 heap.print()
 export {}
