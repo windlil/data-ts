e904c276d63a0c8c902ec52089d64a1f7cde892a
windlil <windlil@outlook.com>
2023-09-22 23:31:30 +0800

    feat: update

--- src/JavaScript/反转URL.js
+++ src/JavaScript/反转URL.js
@@ -8,8 +8,30 @@ function reversedStr(str) {
 }
 
 // 不能使用join 和 split
-function _reversedStr(str) {
-  
+function _reversedStr(str = '') {
+  const arr = []
+  let _str = ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== '.') {
+      _str += str[i]
+    } else {
+      arr.push(_str)
+      _str = ''
+    }
+    if (i === str.length - 1) {
+      arr.push(_str)
+    }
+  }
+
+  let reverse = ''
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (i === 0) {
+      reverse += `${arr[i]}`
+      break
+    }
+    reverse += `${arr[i]}.`
+  }
+  return reverse
 }
 
 console.log(reversedStr(str))
